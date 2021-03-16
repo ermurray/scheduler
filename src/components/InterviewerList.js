9fdadd6344c51d53c2c53d376d61577ab0dbcf4c
Eric Murray <ermurray31@gmail.com>
2021-03-16 17:03:20 -0600

    build component for InterviewListItem

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'components/InterviewerList.scss'
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        <InterviewerListItem
+        
+        />
+      </ul>
+    </section>
+  );
+}
